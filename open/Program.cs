02cb784d6a8a69a829c2e43ccac2c48fca49aec9
Ben Kiefer <kieferfam@msn.com>
2014-05-09 02:30:22 +0100

    simple open project that mimics a little bit of macs open functionality

---
+++ open/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.IO;
+using open.openers;
+
+namespace open
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) { 
+                Debug.WriteLine("Please specify a file");
+                return;
+            }
+
+            Process(args);
+        }
+
+        public static void Process(string[] args) {
+            if (args.Length == 1)
+            {
+                Open(null, args[0]);
+            }
+            else
+            {
+                Open(args[0], args[1]);
+            }
+        }
+
+        public static void Open(string command, string target) {
+            var openers = new Opener[] { new Explorer(), new Editor() };
+
+            foreach (var opener in openers)
+            {
+                if (opener.CanProcess(command))
+                {
+                    opener.Open(target);
+                }
+            }        
+        }
+    }
+
+}
---
+++ open/openers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace open.openers
+{
+    public abstract class Opener {
+        abstract public bool CanProcess(string option);
+        abstract protected string GetCommand();
+
+        public void Open(string target) {
+            var info = new FileInfo(target);
+
+            if (!info.Exists) {
+                info = new FileInfo(Path.Combine(".", target));
+            }
+            
+            Process.Start(GetCommand(), info.FullName);
+        }
+    }
+
+    public class Explorer : Opener {
+        public override bool CanProcess(string option)
+        {
+            return option == null;
+        }
+
+        protected override string GetCommand()
+        {
+            return "explorer";    
+        }
+    }
+
+    public class Editor : Opener
+    {
+        public override bool CanProcess(string option)
+        {
+            return !string.IsNullOrEmpty(option) && option.Equals("-e", System.StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        protected override string GetCommand()
+        {
+            var editor = System.Environment.GetEnvironmentVariable("EDITOR");
+            if (string.IsNullOrEmpty(editor)) {
+                return "notepad.exe";
+            }
+            return editor;
+        }
+    }
+
+}
